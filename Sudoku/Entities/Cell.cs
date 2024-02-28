8b7a42428acc490f16b90b939b25e921780f84a8
J-S-James <jordanjames_tmo@hotmail.com>
2024-02-28 22:15:13 +0000

    Implement Cell and Grid entities

---
+++ Sudoku/Entities/Cell.cs
@@ -0,0 +1,27 @@
+﻿namespace Sudoku.Domain.Entities;
+public struct Cell
+{
+    private int? _digit;
+
+    public Cell(int? digit = null)
+    {
+        _digit = digit;
+    }
+
+    public int? Digit
+    {
+        get => _digit;
+        set
+        {
+            if (value > 0 && value < 9)
+            {
+                _digit = value;
+            }
+        }
+    }
+
+    public void RemoveDigit()
+    {
+        _digit = null;
+    }
+}
---
+++ Sudoku/Entities/Grid.cs
@@ -0,0 +1,27 @@
+﻿namespace Sudoku.Domain.Entities;
+public struct Grid
+{
+    private Cell[,] _cells = new Cell[9, 9];
+
+    public Grid(Cell[,] cells)
+    {
+        if (cells.GetLength(0) != 8 || cells.GetLength(1) != 8)
+        {
+            throw new ArgumentException("Invalid array dimensions. The array must be 9x9");
+        }
+
+        _cells = cells;
+    }
+
+    public Cell GetCell(int x, int y)
+    {
+        if (x < 0 || x > 8 || y < 0 || y > 8)
+        {
+            throw new ArgumentException("Invalid cell coordinates. Coords must be between 0,0 and 8,8");
+        }
+        else
+        {
+            return _cells[x, y];
+        }
+    }
+}
