3049d02127fb440593f812c390f05c27fa66e185
J-S-James <jordanjames_tmo@hotmail.com>
2024-03-11 14:47:19 +0000

    Add all

---
+++ Sudoku.Presentation/Program.cs
@@ -0,0 +1,73 @@
+﻿using Sudoku.Application.Services;
+using Sudoku.Domain.Entities;
+using Sudoku.Domain.Interfaces;
+
+namespace Sudoku.Presentation;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        ISudokuService service = new SudokuService();
+        var watch = System.Diagnostics.Stopwatch.StartNew();
+        var grid = service.GenerateSolution();
+        watch.Stop();
+        Console.WriteLine(watch.ElapsedMilliseconds);
+        PrintBoard(grid);
+    }
+
+    private static void PrintBoard(Grid grid)
+    {
+        PrintGridLine();
+        var CellRowArray = new Square[9];
+        for (int i = 0; i < 9; i++)
+        {
+            for (int column = 0; column <9; column++)
+            {
+                CellRowArray[column] = grid.GetSquare(i, column);
+            }
+
+            PrintNumbersLine(CellRowArray);
+            if ((i + 1) % 3 == 0)
+            {
+                PrintGridLine();
+            }
+        }
+    }
+
+    private static void PrintGridLine()
+    {
+        Console.Write("+");
+        for (int i = 0; i < 3; i++)
+        {
+            Console.Write("---------");
+            Console.Write("+");
+        }
+
+        Console.WriteLine();
+    }
+
+    private static void PrintNumbersLine(Square[] CellRowArray)
+    {
+        Console.Write("|");
+        for (int y = 0; y < 9; y++)
+        {
+            Console.Write(" ");
+            var digit = CellRowArray[y].Digit;
+            if (digit != null)
+            {
+                Console.Write(digit);
+            }
+            else
+            {
+                Console.Write(" ");
+            }
+            Console.Write(" ");
+            if ((y + 1) % 3 == 0)
+            {
+                Console.Write("|");
+            }
+        }
+        Console.WriteLine();
+    }
+}
---
+++ Sudoku/Entities/Square.cs
@@ -0,0 +1,28 @@
+﻿namespace Sudoku.Domain.Entities;
+public class Square
+{
+    private int? _digit;
+    private List<int>? _pencilMarks;
+
+    public Square(int? digit = null)
+    {
+        _digit = digit;
+    }
+
+    public int? Digit
+    {
+        get => _digit;
+        set
+        {
+            if (value > 0 && value <= 9 || value is null)
+            {
+                _digit = value;
+            }
+        }
+    }
+
+    public List<int> PencilMarks
+    {
+        get => _pencilMarks ??= new List<int>();
+    }
+}
